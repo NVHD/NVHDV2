9c6015ce376215e3e3a5c60810930715a522d7fc
Kai Roth <kai@kana.jetzt>
2022-06-15 15:45:22 +0200

    ✔ termine on frontpage

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,19 @@
 import {client} from '$lib/sanityClient'
 
 export async function get() {
-  const data = await client.fetch(`*[_type == "imageComp" && place[0] == "index"]{figuren[]->}`)
+  const data = {}
+  data.figuren = await client.fetch(
+    `*[_type == "imageComp" && place[0] == "index" || _type == "imageComp"]{figuren[]->}`
+  )
+  data.termine = await client.fetch(
+    `*[_type == "termin"]| order(dateTime(date) asc)[0...4]  {date, dateText, description, featured, image, location, locationName, slug, title}`
+  )
 
   if (data) {
     return {
       status: 200,
       body: {
-        figuren: data
+        data
       }
     }
   }
