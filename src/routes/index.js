000a06c5ee58fb2e947c663314c877baf1af8b66
Kai Roth <kai@kana.jetzt>
2022-06-15 15:55:10 +0200

    🔨 ok all fixed now?

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,11 +6,7 @@ export async function get() {
     `*[_type == "imageComp" && place[0] == "index" || _type == "imageComp"]{figuren[]->}`
   )
   data.termine = await client.fetch(
-<<<<<<< HEAD
     `*[_type == "termin"]| order(dateTime(date) asc)[0...4]  {date, dateText, description, featured, image, location, locationName, slug, title}`
-=======
-    `*[_type == "termin"][0...4] | order(dateTime(date) asc) {date, dateText, description, featured, image, location, locationName, slug, title}`
->>>>>>> 26e9f54f5a51f895305d7f7cc1a0cb2164a3c655
   )
 
   if (data) {
